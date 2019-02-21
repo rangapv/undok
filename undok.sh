091a98a986f44f8249f1bb97683c0ae2d3913ead
rangapv <rangapv@yahoo.com>
2019-02-21 07:44:33 +0000

    uninstall docker for fedora for now

---
+++ undok.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+li=$(uname -s)
+li2="${li,,}"
+
+u1=$(cat /etc/*-release | grep ubuntu)
+f1=$(cat /etc/*-release | grep ID= | grep fedora)
+c1=$(cat /etc/*-release | grep ID= | grep centos)
+s1=$(cat /etc/*-release | grep suse)
+
+
+if [ ! -z "$u1" ]
+then 
+	mi=$(lsb_release -cs)
+	mi2="${mi,,}"
+	ji=$(cat /etc/*-release | grep DISTRIB_ID | awk '{split($0,a,"=");print a[2]}')
+	ki="${ji,,}"
+
+	if [ "$ki" == "ubuntu" ]
+	then
+        echo "IT is Ubuntu"
+        fi
+
+
+fi
+
+if [ ! -z "$f1" ]
+then
+        echo "It is Fedora $f1"
+        ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"=");print a[2]}')
+        ki="${ji,,}"
+	cm1="dnf -y"
+        sudo dnf -y remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+fi #end of fedora
